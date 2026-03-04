db5dfd6956bd4b94c737e7cd3c3f4892332bc430
Elysabeth Patrigot <epatrigot@icloud.com>
2026-03-04 10:39:37 +0100

    add projects

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,26 +1,36 @@
 import Title from "./Title"
 
 import app from '../assets/projects/app.jpg'
+import studiolys from '../assets/projects/studiolys.png'
 import gamejam from '../assets/projects/gamejam.webp'
 import scss from '../assets/projects/scss.webp'
 
-import { Github, Video } from "lucide-react";
+import { Github, Eye } from "lucide-react";
 
 
 const projects = [
     {
         id: 1,
-        title: "Wellbeing mobile app",
-        description: "A mobile application designed to help users track and improve their mental health and overall wellbeing.",
+        title: "Application mobile Bien-être",
+        description: "Application mobile conçue pour aider les utilisateurs à suivre et améliorer leur santé mentale ainsi que leur bien-être global.",
         technologies: ["Flutter", "Firebase", "Dart"],
         demoLink: '#',
         repoLink: 'https://github.com/elysabeth-studiolys/studiolys-app',
         image: app,
     },
     {
+        id: 4,
+        title: "Site web de freelance",
+        description: "Site web de freelance conçu pour présenter les services & obtenir des clients.",
+        technologies: ["React", "Tailwind CSS", "TypeScript"],
+        demoLink: '#',
+        repoLink: 'https://github.com/elysabeth-studiolys/studiolys-app',
+        image: studiolys,
+    },
+    {
         id: 2,
         title: "Platformer 2D",
-        description: "A 2D platformer built around temperature-based mechanics — freeze, melt, and overheat to progress.",
+        description: "Un plate-former 2D construit autour de mécaniques basées sur la température — geler, fondre et surchauffer pour progresser.",
         technologies: ["C#", "Unity"],
         demoLink: '#',
         repoLink: 'https://github.com/elysabeth-studiolys/GameJam-Terminus',
@@ -29,7 +39,7 @@ const projects = [
     {
         id: 3,
         title: "From CSS to SCSS",
-        description: "School project developed as part of learning SCSS fundamentals and styling architecture.",
+        description: "Projet scolaire développé dans le cadre de l'apprentissage des fondamentaux de SCSS et de l'architecture de style.",
         technologies: ["HTML", "SCSS", "Dart Sass"],
         demoLink: '#',
         repoLink: 'https://github.com/elysabeth-studiolys/School-project-scss',
@@ -41,9 +51,9 @@ const Projects = () => {
   return (
     <div className="mt-10 flex flex-col gap-20 mb-50" id="Projects">
       <Title title="Last Projects" />
-      <div className="grid md:grid-cols-3 gap-4">
+      <div className="grid md:grid-cols-3 gap-12">
         {projects.map((project) => (
-            <div key={project.id}className="bg-base-300 p-5 h-fit rounded-xl shadow-lg">
+            <div key={project.id}className="card-glass h-full rounded-xl shadow-lg">
                 <img src={project.image} alt={project.title} className="w-full rounded-xl h-56 object-cover" />
                 <div>
                     <h2 className="my-2 font-bold">
@@ -60,12 +70,12 @@ const Projects = () => {
                     ))}
                 </div>
                 <div className="flex gap-2">
-                    <a className="hidden btn btn-primary w-2/3" href={project.demoLink}>
-                        <Video className="w-4" />
-                        Demo
+                    <a className="btn  btn-accent w-1/3" href={project.demoLink}>
+                        <Eye className="w-4" />
+                        Voir
                         
                     </a>
-                    <a className="btn btn-soft btn-accent  w-1/1" href={project.repoLink}>
+                    <a className="btn btn-soft btn-accent  w-2/3" href={project.repoLink}>
                         <Github className="w-4" />
                         GitHub
                         
