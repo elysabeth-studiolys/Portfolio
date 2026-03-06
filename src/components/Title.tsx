d3c1423aa6de1ced74bd28b177dfc59a3155ef9e
Elysabeth Patrigot <epatrigot@icloud.com>
2026-03-06 11:40:32 +0100

    responsive

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -4,7 +4,7 @@ interface TitleProps{
 const Title = ({title} : TitleProps) => {
   return (
     <div className="flex items-center justify-center">
-        <h1 className="gradient-brand-text uppercase font-bold mb-5 text-4xl md:text-5xl ">
+        <h1 className="gradient-brand-text uppercase font-bold mb-5 text-3xl md:text-5xl ">
             {title}
         </h1>
     </div>
